ebd002f6fb4bc902f2dfee1037fdab5e34aaa255
spamfree <spamfreeme2@gmail.com>
2020-08-14 23:45:33 +0530

    time added | basic web

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,22 @@
+// Time
 setInterval(function(){
-    var mydate=new Date()
-    document.getElementById('time').innerHTML=mydate},1000)
+var hours=new Date().getHours()
+if (hours>=12){
+    var ampm='PM'
+}
+else{
+    var ampm= 'AM'
+}
+hours=hours%12
+if (hours==0){hours=12}
+var minutes= new Date().getMinutes()
+if (minutes<10)
+    {minutes=''+'0'+minutes}
+var time= 'Time: '+hours+':'+ minutes+ ' ' + ampm
+document.getElementById('time').innerHTML=time},1000)
 
 
-
-
+    $("#image-1").hover(function(){ var v = document.getElementById("image-1"); 
+    v.className += "image-hover";}, function(){
+        $(this).removeClass("image-hover");
+      });
