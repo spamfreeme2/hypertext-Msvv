7b9e5dbf12959eb1a2f6d53eb0cb916f3ff15c21
spamfree <spamfreeme2@gmail.com>
2020-08-15 16:22:33 +0530

    deleted font awesome and hover added
    deleted font awesome
    @sahilkhandelwal1234  added hover function and resume button

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
 // Time
+$( document ).ready(function() {
+    $("center-Img").hide()
+});
 setInterval(function(){
-var hours=new Date().getHours()
-if (hours>=12){
-    var ampm='PM'
-}
-else{
-    var ampm= 'AM'
-}
-hours=hours%12
-if (hours==0){hours=12}
-var minutes= new Date().getMinutes()
-if (minutes<10)
-    {minutes=''+'0'+minutes}
-var time= 'Time: '+hours+':'+ minutes+ ' ' + ampm
-document.getElementById('time').innerHTML=time},1000)
-
-
-    $("#image-1").hover(function(){ var v = document.getElementById("image-1"); 
-    v.className += "image-hover";}, function(){
-        $(this).removeClass("image-hover");
-      });
+    var hours=new Date().getHours()
+    if (hours>=12){
+        var ampm='PM'
+    }
+    else{
+        var ampm= 'AM'
+    }
+    hours=hours%12
+    if (hours==0){hours=12}
+    var minutes= new Date().getMinutes()
+    if (minutes<10)
+        {minutes=''+'0'+minutes}
+    var time= 'Time: '+hours+':'+ minutes+ ' ' + ampm
+    document.getElementById('time').innerHTML=time},1000)
+    
+    
+    $( ".image-hover" ).hover(
+      function() {
+        $(".center-Img").show() ;
+      }, function() {
+        $(".center-Img").hide();
+      }
+    );
