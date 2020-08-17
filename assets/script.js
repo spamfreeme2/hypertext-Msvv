47e9c3d3de7a2acf34ceafe9cde13fb32573e40b
spamfree <spamfreeme2@gmail.com>
2020-08-17 15:03:19 +0530

    --------

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,42 @@
 // //hiding the text at the start
 // $("body").removeClass('bg-light').addClass('bg-dark');
-$("#unmuted").hide()
+$("#unmuted").hide();
 
 // Time
-setInterval(function(){
-    var hours=new Date().getHours()
-    if (hours>=12){
-        var ampm='PM'
-    }
-    else{
-        var ampm= 'AM'
-    }
-    hours=hours%12
-    if (hours==0){hours=12}
-    var minutes= new Date().getMinutes()
-    var seconds= new Date()
-    if (minutes<10)
-        {minutes=''+'0'+minutes}
-    var time= hours+':'+ minutes+ ' ' + ampm
-    document.getElementById('time').innerHTML=time},1000)
+setInterval(function () {
+	var hours = new Date().getHours();
+	if (hours >= 12) {
+		var ampm = "PM";
+	} else {
+		var ampm = "AM";
+	}
+	hours = hours % 12;
+	if (hours == 0) {
+		hours = 12;
+	}
+	var minutes = new Date().getMinutes();
+	var seconds = new Date();
+	if (minutes < 10) {
+		minutes = "" + "0" + minutes;
+	}
+	var time = hours + ":" + minutes + " " + ampm;
+	document.getElementById("time").innerHTML = time;
+}, 1000);
 
 //Show resume on hovering
 
 //Microphone Animation
 
-$("#unmuted").click(function(){
-  $(this).hide()
-  $("#muted").show()
-})
-$("#muted").click(function(){
-  $(this).hide()
-  $("#unmuted").show()
-})
+$("#unmuted").click(function () {
+	$(this).hide();
+	$("#muted").show();
+});
+$("#muted").click(function () {
+	$(this).hide();
+	$("#unmuted").show();
+});
 
 //tooltips
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-
+	$('[data-toggle="tooltip"]').tooltip();
+});
