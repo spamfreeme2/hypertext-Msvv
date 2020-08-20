07ec0f2b84d850a7fc38edcd3107bf2f628a6024
spamfree <spamfreeme2@gmail.com>
2020-08-21 02:12:02 +0530

    Img change and Trailer change working

--- assets/change.js
+++ assets/change.js
@@ -1,66 +1,25 @@
-const dict = [
-	{
-		id: "1",
-		src: "utilities/games/last_of_us_2.png",
-		txt: "Last of us 2",
-		trailer:"https://www.youtube.com/watch?v=TXl9GI1p_Os&t=15s",
-	},
-	{
-		id: "2",
-		src: "utilities/games/cooking-sim.jpg",
-		txt: "Cooking Simulator",
-		trailer:"https://www.youtube.com/embed/umjU1LpbkZ0",
-	},
-	{
-		id: "3",
-		src: "utilities/games/Cyberpunk-2077.jpg",
-		txt: "Cyberpunk",
-		trailer:"https://www.youtube.com/embed/umjU1LpbkZ0",
-	},
-	{
-		id: "4",
-		src: "utilities/games/uncharted-4.jpg",
-		txt: "Uncharted 4",
-		trailer:"https://www.youtube.com/embed/9ewiJJe_nYI",
-	},
-	
-	{
-		id: "5",
-		src: "utilities/games/NFS-Heat.jpg",
-		txt: "NFS-heat",
-		trailer:""
-	},
-	{
-		id: "6",
-		src: "utilities/games/rdr2-img.jpg",
-		txt: "RDR2",
-		trailer:"https://www.youtube.com/embed/eaW0tYpxyp0",
-	},
-	{
-		id: "7",
-		src: "utilities/games/spiderman.jpg",
-		txt: "Spiderman",
-		trailer:""
-	},
-];
-
-
 $('.changeio').click(function(){
 	let mainStateIMg={
 		elem:$("#1"),
 		imgsrc:$("#1img").attr("src"),
-		txt:$(".maintxt").html()
+		txt:$(".maintxt").html(),
+		trailer:$(".trailerplace").attr('src')
 	}
 	var clickedImg=$(this).find('img').attr('src');
 	var textElem=$(this).find("h4");
 	var clickedImgId=$(this).find('img').attr('id');
 	var clickedImgtext=$(this).find('h4').text();
-	//alert(clickedImgtext);
+	var Trailersrc=$(this).find('h2').text();
+	
 	$(this).find('img').attr('src',mainStateIMg.imgsrc )
 	$(this).find('h4').text(mainStateIMg.txt)
 	console.log(mainStateIMg.img);
 	$("#1").find('img').attr("src",clickedImg);
 	$(".maintxt").html(clickedImgtext)
 	
-												
+	//trailers
+	$(this).find('h2').html(mainStateIMg.trailer)
+	
+	$(".trailerplace").attr('src',Trailersrc)
+
 })
