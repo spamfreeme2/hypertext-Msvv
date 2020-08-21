bea311621dcce33db5193fd9425949e5b4439c54
spamfree <spamfreeme2@gmail.com>
2020-08-21 16:46:22 +0530

    new img added | Yt links

--- assets/change.js
+++ assets/change.js
@@ -1,26 +1,26 @@
-$('.changeio').click(function(){
-	let mainStateIMg={
-		elem:$("#1"),
-		imgsrc:$("#1img").attr("src"),
-		txt:$(".maintxt").html(),
-		trailer:$(".trailerplace").attr('src')
-	}
-	var clickedImg=$(this).find('img').attr('src');
-	var textElem=$(this).find("h4");
-	var clickedImgId=$(this).find('img').attr('id');
-	var clickedImgtext=$(this).find('h4').text();
-	var Trailersrc=$(this).find('h2').text();
-	
-	$(this).find('img').attr('src',mainStateIMg.imgsrc )
-	$(this).find('h4').text(mainStateIMg.txt)
-	console.log(mainStateIMg.img);
-	$("#1").find('img').attr("src",clickedImg);
-	$(".maintxt").html(clickedImgtext)
-	
-	//trailers
-	$(this).find('h2').html(mainStateIMg.trailer)
-	
-	$(".trailerplace").attr('src',Trailersrc)
+$("#4", "#5", "#6").hide();
 
-	
-})
+$(".changeio").click(function () {
+	let mainStateIMg = {
+		elem: $("#1"),
+		imgsrc: $("#1img").attr("src"),
+		txt: $(".maintxt").html(),
+		trailer: $(".trailerplace").attr("src"),
+	};
+	var clickedImg = $(this).find("img").attr("src");
+	var textElem = $(this).find("h4");
+	var clickedImgId = $(this).find("img").attr("id");
+	var clickedImgtext = $(this).find("h4").text();
+	var Trailersrc = $(this).find("h2").text();
+
+	$(this).find("img").attr("src", mainStateIMg.imgsrc);
+	$(this).find("h4").text(mainStateIMg.txt);
+	console.log(mainStateIMg.img);
+	$("#1").find("img").attr("src", clickedImg);
+	$(".maintxt").html(clickedImgtext);
+
+	//trailers
+	$(this).find("h2").html(mainStateIMg.trailer);
+
+	$(".trailerplace").attr("src", Trailersrc);
+});
